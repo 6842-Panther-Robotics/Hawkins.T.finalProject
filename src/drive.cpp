d722358c23f6e2281e157a5204e217ae64cbea65
thawkinsGitHub <63420822+thawkinsGitHub@users.noreply.github.com>
2020-04-09 16:13:37 -0400

    initial commit

---
+++ src/drive.cpp
@@ -0,0 +1,6 @@
+#include "main.h"
+
+pros::Motor rightFront(1, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor rightBack(2, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor leftFront(3, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor leftBack(4, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_DEGREES);
